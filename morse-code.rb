2dd276f92c7f460eed4827043e1ef61ee811e0c6
imubarak234 <imubarak2424@gmail.com>
2022-02-25 10:02:24 +0100

    two methods

---
+++ morse-code.rb
@@ -0,0 +1,29 @@
+DATA = [["A", ".-"], ["B","-..."], ["C","-.-."], ["D","-.."], ["E","."],["F","..-."],["G","--."],
+["H","...."],["I",".."],["J",".---"],["K","-.-"], ["L",".-.."],["M","--"],["N","-."],["O","---"],
+["P",".--."],["Q","--.-"],["R",".-."],["S","..."],["T","-"], ["U","..-"],["V","...-"],["W",".--"],["X", "-..-"],["Y","-.--"],["Z","--.."]]
+
+
+def decode_char(args)
+ans = ''
+DATA.each_with_index {
+  |n, idx| 
+  if n[1] == args
+    ans = n[0] 
+    break
+  end
+}
+return ans
+end
+
+puts decode_char("-..-")
+def decode_word(args)
+  answer = ''
+  ans = args.split(/ /)
+  ans.each{
+    |n| 
+    answer += decode_char(n)
+  }
+  return answer
+end
+
+puts decode_word("-- -.--")
